1b7a1de30a230a3db24064e2edad89cddb340b27
austin_foley <austfoley@gmail.com>
2025-05-13 11:32:39 -0700

    Added Email and Resume

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -35,10 +35,21 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://github.com/vercel/next.js"
+            href="https://github.com/austinfoley"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">github</p>
+          </a>
+        </li>
+            <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="mailto:austfoley@gmail.com"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">email</p>
           </a>
         </li>
         <li>
@@ -46,7 +57,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+            href="app/assets/resume/Austin_Foley_2025_MASTERResume_WEBDEV.pdf"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">resume</p>
