1dd0ffb7ac86743777f921ecc9944ad81a19041b
austin_foley <austfoley@gmail.com>
2025-05-14 14:51:53 -0700

    Changed Experience

---
+++ app/components/musicplayer.tsx
@@ -0,0 +1,12 @@
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+
+export default function MusicPlayer() {
+  return (
+    <AudioPlayer
+      src="/music/Stax Menu Theme 1.23.24.m4a"
+      autoPlay
+      loop
+    />
+  );
+}
